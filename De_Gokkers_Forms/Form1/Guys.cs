552f66035b5333a64785b2070bca06ad9bfc39ec
Patrick-van-Halm <pvanhalm00@gmail.com>
2017-02-14 13:53:01 +0100

    Bets + Guys - Patrick

---
+++ De_Gokkers_Forms/Form1/Guys.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Form1
+{
+    class Guys : Bets
+    {
+        string name;
+        int money;
+
+        public Guys(string name, int money = 50) : base()
+        {
+            this.name = name;
+            this.money = money;
+        }
+
+        //Methods
+
+        public string GetName()
+        {
+            return this.name;
+        }
+
+        public int GetMoney()
+        {
+            return this.money;
+        }
+    }
+}
