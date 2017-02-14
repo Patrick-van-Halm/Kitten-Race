93852d1aa1eacd573dea93d6c9ec1ca7507539a0
VerusLucian <minelukasz@gmail.com>
2017-02-14 14:34:44 +0100

    update kit

---
+++ De_Gokkers_Forms/Form1/kat.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Form1
+{
+    class Kat
+    {
+        private bool    isAlive;
+        private bool    disqualified;
+        private int     position;
+        public Kat()
+        {
+            this.isAlive        = true;
+            this.position       = 0;
+            this.disqualified   = false;
+        }
+        public void Run(int distens)
+        {
+            this.position += distens;
+        }
+        public int Fire(Kat[] Kittens)
+        {
+            int temp = 0;
+            for (int i = 0; i < Kittens.Length; i++)
+            {
+                if (Kittens[i].GetPosition()>= temp)
+                {
+                    temp = Kittens[i].GetPosition();
+                }
+             }
+            for (int i = 0; i < Kittens.Length; i++)
+            {
+                if (temp == Kittens[i].GetPosition())
+                {
+                    this.disqualified = true;
+                    return i;
+                }
+            }
+            return 0;
+        }
+        public bool GetIsAlive()
+        {
+            return this.isAlive;
+        }
+        public int GetPosition()
+        {
+            return this.position;
+        }
+        public bool GetDisqualified()
+        {
+            return this.disqualified;
+        }
+        public void SetIsAlive()
+        {
+            this.isAlive = false;
+        }
+    }
+}
